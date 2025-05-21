775251dc9293d67f5a53bbf92a3f036d3d8e2e1e
rijanswar00 <rijanswar0@gmail.com>
2025-05-21 21:57:02 +0545

    navbar Complete

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils"
+import { Menu, X } from "lucide-react";
 import { useEffect, useState } from "react"
 
 const navItems = [
@@ -12,6 +13,7 @@ const navItems = [
 
 const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -43,6 +45,32 @@ const Navbar = () => {
                 </div>
 
                 {/* Mobile nav  */}
+
+                <button
+                    onClick={() => setIsMenuOpen(prev => !prev)}
+                    className="cursor-pointer md:hidden p-2 text-foreground z-50"
+                    aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
+                >
+                    {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+                </button>
+
+                <div className={cn(
+                    "fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
+                    "transition-all duration-300 md:hidden",
+                    isMenuOpen
+                        ? "opacity-100 pointer-events-auto"
+                        : "opacity-0 pointer-events-none"
+                )}>
+                    <div className="flex flex-col text-xl space-y-8">
+                        {navItems.map((navItem, index) => (
+                            <a
+                                href={navItem.href}
+                                key={index}
+                                className="text-foreground/80 hover:text-primary transition-colors duration-300 "
+                                onClick={() => setIsMenuOpen(false)}>{navItem.name}</a>
+                        ))}
+                    </div>
+                </div>
             </div>
 
         </nav>
