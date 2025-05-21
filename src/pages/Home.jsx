857ea6d36e5bdc3574d8a2fc255ebb57323fbdeb
rijanswar00 <rijanswar0@gmail.com>
2025-05-21 11:01:28 +0545

    star Backgroud done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
+import StarBackground from "../Components/StarBackground"
 import ThemeToggle from "../Components/ThemeToggle"
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle */}
-        <ThemeToggle />
+      <ThemeToggle />
       {/* Background Effects */}
-
+      <StarBackground />
       {/* Navbar */}
 
       {/* Main Content */}
