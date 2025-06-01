72b14f4993d19ff8e32683e7d4b608b584bece2c
rijanswar00 <rijanswar0@gmail.com>
2025-06-01 23:46:10 +0545

    contact section completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/home"
 import NotFound from "./pages/NotFound"
 import "./index.css"
+import { Toaster } from "./Components/ui/toaster"
 
 function App() {
 
   return (
     <>
+    <Toaster />
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />}/>
